91bdcd7f829488f5de48c0a1c5c3b79f7f2814d3
BELGUESMI Aymen <77270008+Aymen491@users.noreply.github.com>
2021-03-11 12:13:51 +0100

    Update middlewere.js

--- pages/middlewere.js
+++ pages/middlewere.js
@@ -1,24 +1,11 @@
-module.exports = function valitime(req,res,next){
-   
-   const time = Date.now;
-  if(time < '09:00:00' && time > '17:00:00' ){
-      res.status(200);
-      next() ;
-  }
-  else{
-      res.status(400).json ("page non dsponible actuellement")
-  }
-}
-
-// module.exports = function valiiday(req , res , next){
-//     const date = getday();
-//     if ( date == '0' && date == '1' ){
-//         res.status (200);
-//         next() ;
-
-//     } else{
-//         res.status (500).json ()
-
-//     }
-
-// }
+module.exports  = function valitime (req, res, next) {
+    let date = new Date();
+    let day = date.getDay();
+    let hours = date.getHours();
+    if (day != 6 && day != 0 && hours > 8 && hours < 17) {
+        res.status(200);
+        next();
+    } else {
+        next(res.send("<center><h1>Cette page disponible de lundi à vendredi  </h1></center> <br> <center><h1>de 09:00h à 17:00h</h1></center>"));
+    }
+};
